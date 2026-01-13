e2bedbce88864c20589f88612dc394fbcf33b9cc
dobri-popov <dobri.popov@gmail.com>
2026-01-13 22:11:07 +0200

    Initial commit (copy from the original task)

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Backend;
+using System;
+using System.Data.Common;
+
+Console.WriteLine("Started");
+Console.WriteLine("Getting DB Connection...");
+
+IDbManager db = new SqliteDbManager();
+DbConnection conn = db.GetConnection();
+
+if (conn == null)
+{
+    Console.WriteLine("Failed to get connection");
+}
---
+++ SqliteDbManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Common;
+using Microsoft.Data.Sqlite;
+
+namespace Backend;
+
+public class SqliteDbManager : IDbManager
+{
+    public DbConnection GetConnection()
+    {
+        try
+        {
+            var connection = new SqliteConnection("Data Source=citystatecountry.db;Mode=ReadWrite");
+            connection.Open();
+            return connection;
+        }
+        catch(SqliteException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return null;
+        }
+    }
+}
